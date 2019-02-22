fd886c61eeabef6c365461c1dd1a526a20f55d55
huangchixing2 <huangchixing2@huawei.com>
2019-02-22 11:58:58 +0800

    1 remove register and forgetPasswd 2 add login logic 3 add jar for webservice

--- app/src/main/java/com/electric/cet/mobile/android/pq/ui/activity/LoginActivity.java
+++ app/src/main/java/com/electric/cet/mobile/android/pq/ui/activity/LoginActivity.java
@@ -2,13 +2,20 @@ package com.electric.cet.mobile.android.pq.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.electric.cet.mobile.android.pq.R;
 
+import java.io.InputStream;
+
+/**
+ *
+ */
 public class LoginActivity extends Activity implements View.OnClickListener {
     private Button login_bt;
     private TextView regist_tv;
@@ -33,18 +40,47 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         forget_tv.setOnClickListener(this);
     }
 
+
+    /**
+     * @param v
+     *  add login logic
+     *  author huangchixing
+     */
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.login_login:
+                if(TextUtils.isEmpty(username_et.getText())){
+                    Toast.makeText(LoginActivity.this, R.string.phone_num_can_not_be_empty, Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(psw_et.getText())) {
+                    Toast.makeText(LoginActivity.this, R.string.passwd_can_not_be_empty, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
                 finish();
-                break;
-            case R.id.login_regist:
-                break;
-            case R.id.login_forget_psw:
                 break;
              default:
                  break;
         }
     }
+
+
+/**
+ * Created by huangchixing
+ * use
+ */
+    public String getToken(String userName, String pwd, String token)
+    {
+        //自定义方法
+        return token;
+    }
+    public String getAuth(InputStream is, String token)
+    {
+        String result = null;
+        return token;
+    }
+
+
 }
