6361c7c8bfb280e5dd993ed4f44be991c3a57ab3
huangchixing2 <huangchixing2@huawei.com>
2019-02-22 10:49:54 +0800

    webservice

--- app/src/main/java/com/electric/cet/mobile/android/pq/utils/WebServiceUtil.java
+++ app/src/main/java/com/electric/cet/mobile/android/pq/utils/WebServiceUtil.java
@@ -6,41 +6,27 @@ import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
 import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
 public class WebServiceUtil {
     private static SoapObject soapObject;
-    private static Handler handler = new Handler() {
-        @Override
-        public void publish(LogRecord record) {
+    Handler handler;
 
-        }
-
-        @Override
-        public void flush() {
-
-        }
-
-        @Override
-        public void close() throws SecurityException {
-
-        }
-    };
     /**
      * 调用WebService的方法
-     *
      */
-    public static void getWebServiceInfo(String nameSpace, String methodName, final String endpoint, final String s<span style="white-space:pre"> </span>oapAction, final SoapObject soapObject, final OnGetResult onGetResult){
+    public static void getWebServiceInfo(String nameSpace, String methodName, final String endpoint, final String soapAction, final SoapObject soapObject, final OnGetResult onGetResult) {
         //开启子线程进行网络的更新
         new Thread(new Runnable() {
+            public android.os.Handler handler;
+
             @Override
             public void run() {
                 SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
                 envelope.bodyOut = soapObject;
                 //设置是否调用的事dotNet开发的webservice
-                envelope.dotnet = true;
-                HttpTransportSE transport = new HttpTransportSE(endPoint);
-                try{
+                envelope.dotNet = true;
+                HttpTransportSE transport = new HttpTransportSE(endpoint);
+                try {
                     //调用webservice
                     transport.call(soapAction, envelope);
                 } catch (Exception e) {
@@ -49,30 +35,35 @@ public class WebServiceUtil {
                 //获取返回的数据
                 final SoapObject object = (SoapObject) envelope.bodyIn;
                 //回调到主线程中进行UI的更新
-                handler.post(new Runnable(){
+                handler.post(new Runnable() {
+
                     @Override
-                    public void run(){
-                        return onGetResultCode(object);
+                    public void run() {
+                        onGetResult.returnCode(object);
                     }
                 });
+
+
             }
         }).start();
     }
+
     /*
      *回调接口
      *
      */
-    public  interface OnGetResult{
+    public interface OnGetResult {
         public void returnCode(SoapObject object);
     }
+
     /*
      *  单例模式获取到Object的实例
      *
      */
-    public static  SoapObject getObjectInstance(String Space, String method){
-        if(soapObject == null){
-            synchronized (WebServiceUtil.class){
-                if(soapObject == null){
+    public static SoapObject getObjectInstance(String Space, String method) {
+        if (soapObject == null) {
+            synchronized (WebServiceUtil.class) {
+                if (soapObject == null) {
                     soapObject = new SoapObject(Space, method);
                 }
             }
